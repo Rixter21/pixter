265f594acd4156103d3ccb6d88f442105811fd50
Rickey Johnson <mr.rixter@gmail.com>
2022-07-08 16:43:26 +0000

    add &apos;

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Contact = () => {
     return (
       <div className='max-w-[1240px] m-auto p-4 h-screen'>
-          <h1 className='text-2xl font-bold text-center p-4'>Let's work together</h1>
-          <form className='max-w-[600px] m-auto'>
+          <h1 className='text-2xl font-bold text-center p-4'>Let&apos;s work together</h1>
+          <form className='max-w-[600px] m-auto'>https://github.com/Rixter21/pixter.git
               <div className='grid grid-cols-2 gap-2'>
                   <input className='border shadow-lg p-3' type="text" placeholder='Name' />
                   <input className='border shadow-lg p-3' type="email" placeholder='Email' />
